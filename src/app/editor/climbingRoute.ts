d9dc077faa7cb8a0f021a268b630586f552824fe
tayydev <51377975+tayydev@users.noreply.github.com>
2024-04-20 18:30:24 -0400

    i hate example route

--- src/app/editor/climbingRoute.ts
+++ src/app/editor/climbingRoute.ts
@@ -22,23 +22,6 @@ export interface NaturalData {
     hold2id: string
 }
 
-export const exampleRoute: ClimbingRoute = {
-    id: uuidv4(),
-    name: "Sunset Climb",
-    image: "path/to/sunset_climb.jpg",
-    color1: "#FF5733",
-    color2: "#C70039",
-    holds: [
-        { id: uuidv4(), x: 15, y: 20 },
-        { id: uuidv4(), x: 30, y: 50 },
-        { id: uuidv4(), x: 45, y: 75 }
-    ],
-    naturals: [
-        { id: uuidv4(), x1: 60, y1: 100, x2: 65, y2: 105 },
-        { id: uuidv4(), x1: 70, y1: 110, x2: 75, y2: 115 }
-    ]
-};
-
 export const defaultRoute: ClimbingRoute = {
     id: uuidv4(),
     name: "Untitled",
