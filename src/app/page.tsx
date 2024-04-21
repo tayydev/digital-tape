47f2eb8a723a7498b61495637fddaa89fc1599a7
tayydev <51377975+tayydev@users.noreply.github.com>
2024-04-21 08:21:33 -0400

    center a div challenge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,24 @@ import {Box} from "@mui/system";
 export default function Home() {
   return (
     <main>
-        <Stack textAlign={"center"} spacing={4}>
+        <Stack textAlign={"center"} spacing={2}>
             <Box/>
             <Typography variant={"h1"} fontWeight={"bold"}>
                 Settr Pro
             </Typography>
             <Typography variant={"h3"}>
-                Route Preservation? Solved.
+                Route Preservation? Solved. Take a look:
             </Typography>
+            <Box style={{width: "100%"}}>
+                <Box
+                    style={{ border: "2px solid lightgrey", maxWidth: "200mm", margin: "auto" }}
+                >
+                    <video width="100%" height="auto" autoPlay muted loop>
+                        <source src="/demo.mp4" type="video/mp4"/>
+                        Your browser does not support the video tag.
+                    </video>
+                </Box>
+            </Box>
         </Stack>
     </main>
   );
